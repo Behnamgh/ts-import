af815e35c259bdc14b84c653c67d1998db9185e6
Artur Kurowski <radarsu@gmail.com>
2020-09-07 21:24:10 +0200

    chore: remove cache

--- cache/home/radarsu/workspace/radarsu-pnpm/.config/radrat/local.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    app: `core`,
-    externalHost: `192.168.1.11`,
-    platform: `android`,
-};
-exports.default = config;
